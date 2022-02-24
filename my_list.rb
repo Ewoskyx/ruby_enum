6981fce555cf81b904641c3fd23058a0c9e97821
KayLemba <kaylemba@yahoo.com>
2022-02-24 12:28:59 +0200

    Add my_list.rb

---
+++ my_list.rb
@@ -0,0 +1,30 @@
+require_relative 'my_enum'
+
+class MyList
+  @list = []
+  include MyEnumerable
+
+  def each(&block)
+    @list.each(&block)
+  end
+
+  def initialize(*lis)
+    @list = lis
+  end
+end
+
+puts list = MyList.new(1, 2, 3, 4)
+# => #<MyList: @list=[1, 2, 3, 4]>
+
+puts(list.all? { |e| e < 5 })
+# => true
+puts(list.all? { |e| e > 5 })
+# => false
+
+puts(list.any? { |e| e == 2 })
+# => true
+puts(list.any? { |e| e == 5 })
+# => false
+
+puts(list.filter?(&:even?))
+# => [2, 4]
